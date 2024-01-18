cefbaae8ac96c61043aa1c22390c2cabaadcf915
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2024-01-18 16:26:54 +0300

    edit base url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 
-export const BaseURL = `http://localhost:8000`;
+export const BaseURL = `http://api2.qtrendqa.com/`;
 export const BaseURL_IMAGE = BaseURL.slice(0,-1);
 const PROJECT_NAME = "PROJECT_NAME"
 export const TOKEN_KEY = PROJECT_NAME +  "_TOKEN"
