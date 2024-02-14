861deb7ea3a953480f7d8404252f88b096a90d7e
Moaz Dawalibi <moaz.dawalibi@sdnone.net>
2024-02-14 10:24:02 +0300

    ..

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
     
-export const BaseURL = `https://api2.qtrendqa.com/`;
+export const BaseURL = `https://api.karmaantiquecoin.com/`;
 export const BaseURL_IMAGE = BaseURL.slice(0,-1);
 const PROJECT_NAME = "PROJECT_NAME"
 export const TOKEN_KEY = PROJECT_NAME +  "_TOKEN"
